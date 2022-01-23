fd657b55231ceaf755315c2826b631beff27b8bd
BadWolf20 <Roman.Pichugin20@gmail.com>
2022-01-23 14:06:17 +0700

    mainViewController add

--- SettingsMVC/Sources/Scenes/Controller/MainViewController.swift
+++ SettingsMVC/Sources/Scenes/Controller/MainViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var model: SettingsModel?
+
+    private var MainView: MainView? {
+        guard isViewLoaded else { return nil }
+        return view as? MainView
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view = SettingsMVC.MainView()
+        model = SettingsModel()
+        configureView()
+    }
 
 }
+
+private extension MainViewController {
+    func configureView() {
+        guard let models = model?.createModel() else { return }
+        MainView?.configureView(with: models)
+    }
+}
+
 
